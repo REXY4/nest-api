f2a1e4a80b094a6479f102f3715370502f087b3e
rexy4 <muhammadrzki96@gmail.com>
2023-03-10 21:01:53 +0700

    add

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,20 +1,27 @@
-import { IsEmail, IsNotEmpty, MinLength, IsDefined, Matches, IsString } from "class-validator";
+import {
+  IsEmail,
+  IsNotEmpty,
+  MinLength,
+  IsDefined,
+  Matches,
+  IsString,
+} from 'class-validator';
 
 export class CreateUserDto {
-    @IsNotEmpty()
-    @IsDefined()
-    @IsString()
-    @MinLength(3)
-    username: string;
+  @IsNotEmpty()
+  @IsDefined()
+  @IsString()
+  @MinLength(3)
+  username: string;
 
-    @IsNotEmpty()
-    @IsEmail()
-    email: string;
-  
-    @IsNotEmpty()
-    // @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\S+$/)
-    @IsDefined()
-    @IsString()
-    @MinLength(8)
-    password: string;
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\S+$/)
+  @IsDefined()
+  @IsString()
+  @MinLength(8)
+  password: string;
 }
